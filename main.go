d2b1fbc8a85c6c9b9b76b677959c549715ae9edc
Ilham Abdullah <72017753+ilhamabdlh@users.noreply.github.com>
2021-11-08 13:48:24 +0700

    init

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"./configs"
+	"github.com/ilhamabdlh/go-restapi/configes"
 )
 
+type Configs interface{
+	getConfigs()
+	getConfig()
+	createConfig()
+	updateConfigs()
+	deleteConfigs()
+	Configs()
+}
+
+func excecuteConfigs(c Config){
+	c.getConfigs()
+	c.getConfig()
+	c.createConfig()
+	c.updateConfigs()
+	c.deleteConfigs()
+	c.Configs()
+}
 func main() {
-	configs.Configs()
+	excecuteConfigs()
 }
